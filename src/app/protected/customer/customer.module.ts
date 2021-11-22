4c6296673e686f467ad26c88ef5420bdce7ded9e
diana-terinnova <diana.diabong@terinnova.com>
2021-11-22 14:03:52 +0000

    Complete lazy loading configuration

--- src/app/protected/customer/customer.module.ts
+++ src/app/protected/customer/customer.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module'; 
 
 import { CustomerRoutingModule } from './customer-routing.module';
+import { CustomerComponent } from './components/customer/customer.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CustomerComponent
+  ],
   imports: [
     SharedModule,
     CustomerRoutingModule
