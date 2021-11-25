f0db5153e6ad086444cc0e8cc12fd43de5a972a1
diana-terinnova <diana.diabong@terinnova.com>
2021-11-25 16:06:58 +0000

    Dashboard ui v:1

--- src/app/protected/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/protected/dashboard/components/dashboard/dashboard.component.ts
@@ -5,30 +5,81 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
+  cards = [
+    {
+      title: 'Orders',
+      img: '../../../../../assets/orders.png',
+      number: 5122,
+    },
+    {
+      title: 'Total Pending',
+      img: '../../../../../assets/pending.png',
+      number: 145,
+    },
+    {
+      title: 'Total Completed',
+      img: '../../../../../assets/done.png',
+      number: 1204,
+    },
+  ];
 
   /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 2, rows: 1 },
-          { title: 'Card 2', cols: 2, rows: 1 },
-          { title: 'Card 3', cols: 2, rows: 1 },
-          { title: 'Card 4', cols: 2, rows: 1 }
-        ];
-      }
+  // Row1 = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+  //   map(({ matches }) => {
+  //     if (matches) {
+  //       return [
+  //         {
+  //           title: 'Orders',
+  //           cols: 2,
+  //           rows: 1,
+  //           img: '../../../../../assets/orders.png',
+  //           number: 5122,
+  //         },
+  //         {
+  //           title: 'Total Pending',
+  //           cols: 2,
+  //           rows: 1,
+  //           img: '../../../../../assets/pending.png',
+  //           number: 145,
+  //         },
+  //         {
+  //           title: 'Total Competed',
+  //           cols: 2,
+  //           rows: 1,
+  //           img: '../../../../../assets/done.png',
+  //           number: 1204,
+  //         },
+  //       ];
+  //     }
 
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
+  //     return [
+  //       {
+  //         title: 'Orders',
+  //         cols: 1,
+  //         rows: 1,
+  //         img: '../../../../../assets/orders.png',
+  //         number: 5122,
+  //       },
+  //       {
+  //         title: 'Total Pending',
+  //         cols: 1,
+  //         rows: 1,
+  //         img: '../../../../../assets/pending.png',
+  //         number: 145,
+  //       },
+  //       {
+  //         title: 'Total Completed',
+  //         cols: 1,
+  //         rows: 1,
+  //         img: '../../../../../assets/done.png',
+  //         number: 1204,
+  //       },
+  //     ];
+  //   })
+  // );
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 }
