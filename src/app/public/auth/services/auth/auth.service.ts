499267ab2d1c00a656973d4d9495570a0c4b124b
unknown <FGLO>
2021-12-16 14:17:12 +0000

    added logout feature in authService class

--- src/app/public/auth/services/auth/auth.service.ts
+++ src/app/public/auth/services/auth/auth.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import {LoginAdmin} from '../../models/loginAdmin'
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient, private router: Router) {}
 
   login(data: LoginAdmin){
     return this.httpClient.post<LoginAdmin>(`${environment.baseUrl}users/auths/admins/login`, data)
@@ -44,6 +45,11 @@ export class AuthService {
       // return id;
     }
     return 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImZhNjA4OGU5LWZiNTAtNDVjNC1iNTdkLTUxNDg1ZDJlNTY4NiIsImV4cCI6MTYzOTIyNzg4Mn0.A3dSL2GeiTImbVlZ9Z40nuABpnLzsud-JSUuJT3fLvg';
+  }
+
+  logout(){
+    localStorage.removeItem('admin');
+    this.router.navigate(['/login'])
   }
 
 }
