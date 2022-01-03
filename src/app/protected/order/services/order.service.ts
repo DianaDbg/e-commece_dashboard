1197bc28985c19467c06c95bd35c6840e91bbef7
diana-terinnova <diana.diabong@terinnova.com>
2022-01-03 11:41:17 +0000

    Refactor add category

--- src/app/protected/order/services/order.service.ts
+++ src/app/protected/order/services/order.service.ts
@@ -1,60 +1,45 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {Order} from '../models/order';
+import { Order } from '../models/order';
 import { AuthService } from 'src/app/public/auth/services/auth/auth.service';
 import { environment } from 'src/environments/environment';
 import { Response } from 'src/app/shared/models/response/response';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrderService {
- 
-  baseUrl =  `${environment.baseUrl}`+'orders/'
-  
-  constructor(private httpClient: HttpClient, private authService: AuthService) { }
+  baseUrl = `${environment.baseUrl}` + 'orders/';
 
- 
-  getOrders(){
-    return this.httpClient.get<Response<Order[]>>(
-      this.baseUrl+'admins',
-      {
-        headers: new HttpHeaders({
-          Authorization: 'token ' + this.authService.getToken(),
-          'Content-Type': 'application/json',
-        }),
-      }
-    );
+  constructor(
+    private httpClient: HttpClient,
+    private authService: AuthService
+  ) {}
+
+  getOrders() {
+    return this.httpClient.get<Response<Order[]>>(this.baseUrl + 'admins', {
+      headers: new HttpHeaders({
+        Authorization: 'token ' + this.authService.getToken(),
+        'Content-Type': 'application/json',
+      }),
+    });
   }
 
-
-  confirmOrder(order_id: number){
-
-    return this.httpClient.put(
-      this.baseUrl+`${order_id}` + '/confirm',
-      {
-        headers: new HttpHeaders({
-          Authorization: 'token ' + this.authService.getToken(),
-          'Content-Type': 'application/json',
-        }),
-      }
-    );
+  confirmOrder(order_id: number) {
+    return this.httpClient.put(this.baseUrl + `${order_id}` + '/confirm', {
+      headers: new HttpHeaders({
+        Authorization: 'token ' + this.authService.getToken(),
+        'Content-Type': 'application/json',
+      }),
+    });
   }
 
-
-  cancelOrder(order_id: number){
-    return this.httpClient.delete(
-      this.baseUrl+`${order_id}`,
-      {
-        headers: new HttpHeaders({
-          Authorization: 'token ' + this.authService.getToken(),
-          'Content-Type': 'application/json',
-        }),
-      }
-    );
+  cancelOrder(order_id: number) {
+    return this.httpClient.delete(this.baseUrl + `${order_id}`, {
+      headers: new HttpHeaders({
+        Authorization: 'token ' + this.authService.getToken(),
+        'Content-Type': 'application/json',
+      }),
+    });
   }
-
-
-
 }
